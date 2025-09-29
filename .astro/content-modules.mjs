6e83a2bc20e2360f7840069c2b08f39cea7e241c
Ahmath.Gadji <ahmathgadji27@gmail.com>
2025-09-29 12:22:46 +0000

    small update

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -4,9 +4,8 @@ export default new Map([
 ["src/content/docs/license.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Flicense.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/support-and-contribute.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fsupport-and-contribute.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/documentation/API.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fdocumentation%2FAPI.mdx&astroContentModuleFlag=true")],
-["src/content/docs/getting_started/quickstart.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting_started%2Fquickstart.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/getting_started/usage.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting_started%2Fusage.mdx&astroContentModuleFlag=true")],
+["src/content/docs/getting_started/quickstart.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting_started%2Fquickstart.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/installation/ansible_setup.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Finstallation%2Fansible_setup.mdx&astroContentModuleFlag=true")],
-["src/content/docs/installation/docker.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Finstallation%2Fdocker.mdx&astroContentModuleFlag=true")],
-["src/content/docs/documentation/deploy_ray_cluster.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fdocumentation%2Fdeploy_ray_cluster.mdx&astroContentModuleFlag=true")]]);
+["src/content/docs/installation/docker.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Finstallation%2Fdocker.mdx&astroContentModuleFlag=true")]]);
 		
